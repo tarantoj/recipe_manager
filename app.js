b9232adc699a13087520024b3c72b6e485056c3e
James Taranto <tarantoj@gmail.com>
2019-07-02 15:47:22 +1000

    lint

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var recipeRouter = require('./routes/recipe');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const recipeRouter = require('./routes/recipe');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
 
 require('./models/db');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +28,7 @@ app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+  sourceMap: true,
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,12 +36,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cookbook', recipeRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

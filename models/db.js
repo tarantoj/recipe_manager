c774b37674d7779a5116970d3a3219005cfb0d00
James Taranto <tarantoj@gmail.com>
2019-07-02 11:20:16 +1000

    change to personal atlas db

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// TODO: whitelist heroku on atlas
+const ATLAS_USER = process.env.ATLAS_USER || '';
 const ATLAS_PASS = process.env.ATLAS_PASS || '';
-const dbURI = `mongodb+srv://endwaste:${ATLAS_PASS}@cluster0-0uuii.azure.mongodb.net/test?retryWrites=true`;
+const dbURI = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASS}@cluster0-grx4h.mongodb.net/test?retryWrites=true&w=majority`;
 
 const options = {
   useNewUrlParser: true,

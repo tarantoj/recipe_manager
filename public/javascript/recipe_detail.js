6de3bfbc755ec9462880397df77d8dd68f5bd551
James Taranto <taranto.james@gmail.com>
2019-08-30 16:22:07 +1000

    Fix frontend delete functionality

--- public/javascript/recipe_detail.js
+++ public/javascript/recipe_detail.js
@@ -1,5 +1,7 @@
 function deleteRecipe(id) {
-  if(confirm("Are you sure you want to delete this recipe?")) {
-    //Delete
+  if (confirm('Are you sure you want to delete this recipe?')) {
+    $.post(`/cookbook/recipe/${id}/delete`, { id }, (data, status) => {
+      window.location.assign('/cookbook/recipes');
+    });
   }
 }
